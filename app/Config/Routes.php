22bf5abaa5dec198778a2708b46c3d1d5f366b91
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2024-08-28 13:08:01 -0600

    Se termina de definr logica en el controlador Blog.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,6 +10,9 @@ $routes->get('/', 'Home::index');
 $routes->get('/login', 'Login::login');
 $routes->post('/auth', 'Login::auth');
 $routes->get('/logout', 'Login::logout');
-$routes->get('/blogs', 'Blog::index');
-
-
+$routes->get('/blogs', 'Blog::index', ['filter' => 'auth']);
+$routes->get('/blogs/create', 'Blog::create', ['filter' => 'auth']);
+$routes->get('/blogs/store', 'Blog::store', ['filter' => 'auth']);
+$routes->get('/blogs/edit/:num', 'Blog::edit/$i', ['filter' => 'auth']);
+$routes->get('/blogs/update/', 'Blog::update', ['filter' => 'auth']);
+$routes->get('/blogs/delete/:num', 'Blog::delete/$i', ['filter' => 'auth']);
--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -3,12 +3,70 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use CodeIgniter\HTTP\RedirectResponse;
 use CodeIgniter\HTTP\ResponseInterface;
+use ReflectionException;
 
 class Blog extends BaseController
 {
-    public function index()
+    public function index(): string
     {
-        echo "index";
+        $model = new \App\Models\Blog();
+        $data['blogs'] = $model->orderBy('id', 'DESC')->findAll();
+        return view('blog/index', $data);
+    }
+
+    public function create(): string
+    {
+        return view('product/create');
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function store(): RedirectResponse
+    {
+        $model = new \App\Models\Blog();
+        $data = [
+            'users_id' => session()->get('id'),
+            'title' => $this->request->getPost('title'),
+            'content' => $this->request->getPost('content'),
+        ];
+        $model->save($data);
+        session()->setFlashdata('success', 'Create success');
+        return redirect()->to('/blogs');
+    }
+
+    public function edit($id): string
+    {
+        $model = new \App\Models\Blog();
+        $data['blog'] = $model->find($id);
+        return view('blog/edit', $data);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function update(): RedirectResponse
+    {
+        $model = new \App\Models\Blog();
+        $id = $this->request->getPost('id');
+        $data = [
+            'users_id' => session()->get('id'),
+            'title' => $this->request->getPost('title'),
+            'content' => $this->request->getPost('content'),
+        ];
+
+        $model->update($id, $data);
+        session()->setFlashdata('success', 'Update success');
+        return redirect()->to('/blogs');
+    }
+
+    public function delete($id): RedirectResponse
+    {
+        $model = new \App\Models\Blog();
+        $model->delete($id);
+        session()->setFlashdata('success', 'Delete success');
+        return redirect()->to('/blogs');
     }
 }
